b5b46c77db2affcdba03517438575790b1b76d18
Luis Mendoza <luis@mendoza.io>
2016-11-02 18:10:16 -0600

    Update test config

--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,7 @@
 export default {
   env: 'test',
   jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-  // db: 'mongodb://localhost:27017/node-docker-api-development', // localhost
-  db: 'mongodb://mongo:27017/node-docker-api-development', // container
+  // db: 'mongodb://localhost:27017/node-docker-api-test', // localhost
+  db: 'mongodb://mongo:27017/node-docker-api-test', // container
   port: 4040
 };
